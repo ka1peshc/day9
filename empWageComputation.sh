dfefa08780cde951f4f378fd7b29c2eb466f38ff
ka1peshc <kalpesh111997@gmail.com>
2021-08-20 00:29:41 +0530

    Added daily wage code

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,6 +1,13 @@
+perHour=20
+totalSalary=0
+pay=0
 if [ $((RANDOM%2)) -eq 0 ]
 then
     echo "Present"
+    pay=$(($perHour*8))
 else
     echo "Absent"
+    pay=0
 fi
+totalSalary=$(($totalSalary+$pay))
+echo "$totalSalary"
