19f34d3edde352879ee02523ae966a914536fd92
ka1peshc <kalpesh111997@gmail.com>
2021-08-20 02:10:30 +0530

    Stored daily wage and total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,6 +10,9 @@ MAX_WORK_DAYS=20
 hourCount=0
 dayCount=0
 
+dwCount=0
+twCount=0
+
 function getWorkingHours() {
 	case $1 in
 		$isFullTime)
@@ -26,8 +29,12 @@ while [[ $totalWorkingHour -lt $MAX_HOURS && $dayCount -lt $MAX_WORK_DAYS ]]
 do
     ((dayCount++))
     workHours=$( getWorkingHours $((RANDOM%3)) )
+    arrDailyWage[((dwCount++))]=$(($workHours*$WAGE_PER_HOUR))
     totalWorkingHour=$(($totalWorkingHour+$workHours))
+    arrTotalWage[((twCount++))]=$(($totalWorkingHour*$WAGE_PER_HOUR))
 done
 totalSalary=$(($totalWorkingHour*$WAGE_PER_HOUR))
 
 echo "Total salary $totalSalary"
+echo "array of daily wage: "${arrDailyWage[@]}
+echo "array of Total wage: "${arrTotalWage[@]}
