88376f632689be050eb4c115e3f21cf59c1629ad
ka1peshc <kalpesh111997@gmail.com>
2021-08-20 01:31:02 +0530

    Calculate wage till day or working hour condition match

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,18 +1,27 @@
-WageperHour=20
 totalSalary=0
 pay=0
-empAttendence=$((RANDOM%3))
-case $empAttendence in
-    0)
-    pay=$(($WageperHour * 8))
-    echo "Full day";;
-    1)
-    pay=$(($WageperHour * 4))
-    echo "Part time";;
-    2)
-    pay=0
-    echo "Absent";;
-esac
 
-totalSalary=$(($totalSalary+$pay))
-echo "$totalSalary"
+WAGE_PER_HOUR=20
+MAX_HOURS=100
+MAX_WORK_DAYS=20
+
+hourCount=0
+dayCount=0
+
+while [[ $hourCount -lt 100 && $dayCount -lt 20 ]]
+do
+    empAttendence=$((RANDOM%3))
+    case $empAttendence in
+        0)
+            pay=$(($WAGE_PER_HOUR * 8))
+            hourCount=$(($hourCount+8));;
+        1)
+            pay=$(($WAGE_PER_HOUR * 4))
+            hourCount=$(($hourCount+8));;
+        2)
+            pay=0;;
+    esac
+    ((dayCount++))
+    totalSalary=$(($totalSalary+$pay))
+done
+echo "Total working day $dayCount Total working hour $hourCount Total salary $totalSalary"
