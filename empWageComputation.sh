e203a9a5d9773ad458e062eb75cce25bb70b89b3
ka1peshc <kalpesh111997@gmail.com>
2021-08-20 01:10:59 +0530

    Calculating total salary for whole month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,18 +1,24 @@
 WageperHour=20
 totalSalary=0
 pay=0
-empAttendence=$((RANDOM%3))
-case $empAttendence in
-    0)
-    pay=$(($WageperHour * 8))
-    echo "Full day";;
-    1)
-    pay=$(($WageperHour * 4))
-    echo "Part time";;
-    2)
-    pay=0
-    echo "Absent";;
-esac
 
-totalSalary=$(($totalSalary+$pay))
-echo "$totalSalary"
+totalWorkingDays=20
+workdayCount=0
+dayCount=0
+while [[ $workday -le 20 && $dayCount -lt 30 ]]
+do
+    empAttendence=$((RANDOM%3))
+    case $empAttendence in
+        0)
+            ((workdayCount++))
+            pay=$(($WageperHour * 8));;
+        1)
+            ((workdayCount++))
+            pay=$(($WageperHour * 4));;
+        2)
+            pay=0;;    
+    esac
+    ((dayCount++))
+    totalSalary=$(($totalSalary+$pay))
+done
+echo "$dayCount day working days are $workdayCount and Salary got $totalSalary "
