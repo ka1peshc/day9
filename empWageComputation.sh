c695ae4cf5854f7c53978460c1f06d49f6a906a3
ka1peshc <kalpesh111997@gmail.com>
2021-08-20 01:56:52 +0530

    Refactor code into function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,7 @@
 totalSalary=0
-pay=0
+totalWorkingHour=0
+isFullTime=0
+isPartTime=1
 
 WAGE_PER_HOUR=20
 MAX_HOURS=100
@@ -8,20 +10,24 @@ MAX_WORK_DAYS=20
 hourCount=0
 dayCount=0
 
-while [[ $hourCount -lt 100 && $dayCount -lt 20 ]]
+function getWorkingHours() {
+	case $1 in
+		$isFullTime)
+			workHours=8;;
+		$isPartTime)
+			workHours=4;;
+		*)
+			workHours=0;;
+	esac
+echo $workHours
+}
+
+while [[ $totalWorkingHour -lt $MAX_HOURS && $dayCount -lt $MAX_WORK_DAYS ]]
 do
-    empAttendence=$((RANDOM%3))
-    case $empAttendence in
-        0)
-            pay=$(($WAGE_PER_HOUR * 8))
-            hourCount=$(($hourCount+8));;
-        1)
-            pay=$(($WAGE_PER_HOUR * 4))
-            hourCount=$(($hourCount+8));;
-        2)
-            pay=0;;
-    esac
     ((dayCount++))
-    totalSalary=$(($totalSalary+$pay))
+    workHours=$( getWorkingHours $((RANDOM%3)) )
+    totalWorkingHour=$(($totalWorkingHour+$workHours))
 done
-echo "Total working day $dayCount Total working hour $hourCount Total salary $totalSalary"
+totalSalary=$(($totalWorkingHour*$WAGE_PER_HOUR))
+
+echo "Total salary $totalSalary"
