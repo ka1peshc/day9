a870cad40911596a1a1445b57e0fa41d05bfb576
ka1peshc <kalpesh111997@gmail.com>
2021-08-20 16:01:49 +0530

    Added Day count

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 totalSalary=0
 totalWorkingHour=0
 isFullTime=0
@@ -13,17 +14,26 @@ dayCount=0
 dwCount=0
 twCount=0
 
+fullTime=0
+partTime=0
+absent=0
+
 function getWorkingHours() {
 	case $1 in
 		$isFullTime)
-			workHours=8;;
+			workHours=8
+            ((fullTime++));;
 		$isPartTime)
-			workHours=4;;
+			workHours=4
+            ((partTime++));;
 		*)
-			workHours=0;;
+			workHours=0
+            ((absent++));;
 	esac
 echo $workHours
 }
+
+declare -A dailyWage
 
 while [[ $totalWorkingHour -lt $MAX_HOURS && $dayCount -lt $MAX_WORK_DAYS ]]
 do
@@ -32,9 +42,15 @@ do
     arrDailyWage[((dwCount++))]=$(($workHours*$WAGE_PER_HOUR))
     totalWorkingHour=$(($totalWorkingHour+$workHours))
     arrTotalWage[((twCount++))]=$(($totalWorkingHour*$WAGE_PER_HOUR))
+    dailyWage[$dayCount]=$(($workHours*$WAGE_PER_HOUR))
 done
 totalSalary=$(($totalWorkingHour*$WAGE_PER_HOUR))
 
 echo "Total salary $totalSalary"
 echo "array of daily wage: "${arrDailyWage[@]}
 echo "array of Total wage: "${arrTotalWage[@]}
+for (( i=0; i<${#dailyWage[@]}; i++ ))
+do
+    echo "Day "$i" Wage earn "${dailyWage[$i]}
+done
+echo "Total salary $totalSalary"
